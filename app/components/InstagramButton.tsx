4038c2d59251440bc48645ad04809897b5f4de1e
Mitja Belak <mico@0x0.icu>
2023-07-27 16:05:23 +0200

    40px fix?

--- app/components/InstagramButton.tsx
+++ app/components/InstagramButton.tsx
@@ -1,3 +1,11 @@
-export const InstagramButton = () => (<a href="https://www.instagram.com/zelosumljivarastlina" target="_blank" rel="noopener noreferrer" className="relative text-notice">
-    <span className="inline-flex md:inline instagram-button border border-accent rounded my-2 md:m-4">@zelosumljivarastlina</span>
-  </a>)
+export const InstagramButton = () => (
+    <a href="https://www.instagram.com/zelosumljivarastlina"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative text-notice">
+        <span
+            className="inline-flex md:inline instagram-button border border-accent rounded my-2 md:m-4">
+            @zelosumljivarastlina
+        </span>
+    </a>
+)
